e3a3f6b94db497ccd5178272d7d6c847928c92a2
Molina <molina@z1.digital>
2021-11-24 18:41:10 +0100

    feat: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,25 @@
-import { Container } from './styles';
+import NextLink from 'next/link';
 
-const Footer = () => <Container>Footer</Container>;
+import { Container, Github, Linkedin, Twitter } from './styles';
+
+const Footer = () => (
+  <Container>
+    <NextLink passHref href="https://github.com/Josmolmor">
+      <a target="_blank">
+        <Github />
+      </a>
+    </NextLink>
+    <NextLink passHref href="https://www.linkedin.com/in/josmolmor/">
+      <a target="_blank">
+        <Linkedin />
+      </a>
+    </NextLink>
+    <NextLink passHref href="https://twitter.com/JosMolMor">
+      <a target="_blank">
+        <Twitter />
+      </a>
+    </NextLink>
+  </Container>
+);
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,26 @@
-import styled from 'styled-components';
+import { GithubIcon, LinkedinIcon, TwitterIcon } from 'components/Icons';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+const commonIconCss = css`
+  color: ${({ theme }) => theme.colors.white};
+  height: 2rem;
+  width: 2rem;
+`;
+
+export const Github = styled(GithubIcon)`
+  ${commonIconCss};
+`;
+
+export const Linkedin = styled(LinkedinIcon)`
+  ${commonIconCss};
+`;
+
+export const Twitter = styled(TwitterIcon)`
+  ${commonIconCss};
+`;
